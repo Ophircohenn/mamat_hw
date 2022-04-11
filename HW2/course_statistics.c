7d898331ce7a122f9af9bc00090b64b0c5e8e36f
Mamat <ophir.cohen@campus.technion.ac.il>
2022-04-11 16:01:54 +0300

    c

--- HW2/course_statistics.c
+++ HW2/course_statistics.c
@@ -14,13 +14,14 @@ int main(int argc, char* argv[]) {
     fp = fopen("course_statistics.txt", "w");
     fp2= fopen("grades.txt", "r");
     int counter = 0;
+    double pass=0;
     int sum_of_grades = 0;
     int tmp_grade = 0;
     //printf("\n%d", &tmp_grade);
     int hist[101] = {0};
     int min = 100, max = 0, mid = 0,counter2 = 0, counter3 = 0;
     double avg=0;
-    printf("before while\n");
+   // printf("before while\n");
     //int x=fscanf(fp2, "%d", &tmp_grade);
     //printf("x = %d", &tmp_grade);
     while (fscanf(fp2, "%d", &tmp_grade) > 0)
@@ -35,25 +36,39 @@ int main(int argc, char* argv[]) {
         min= (min > tmp_grade ? tmp_grade : min);
         max=(max < tmp_grade ? tmp_grade : max);
     }
-    printf("after while \n %d\n ",sum_of_grades);
+   // printf("after while \n %d\n ",sum_of_grades);
     avg = (double)sum_of_grades /counter;
     for (size_t i = 0; i < 101; i++)
     {
         counter2 += hist[i];
         if (counter2 >= (counter / 2))
         {
-            mid = i;
-            break;
+            mid=i;
+        	break;
         }
     }
     for (size_t i = 55; i < 101; i++)
     {
         counter3 += hist[i];
     }
-    double pass = (double)(counter3 / counter) * 100;
-
+    pass = (double)(((double)(counter3*100))/ counter);
+    //printf("/n--%.2f--\n%d\n%d",pass,counter3,counter);
+    fprintf(fp,"num of students = %d\n",counter);
+    fprintf(fp,"avg = %.3f\n",avg);
+    fprintf(fp,"the median is - %d\n",mid);
+    fprintf(fp,"max grade = %d, min grade = %d\n",max,min);
+    fprintf(fp,"pass rate = %.2f%%\n",pass);
+    for (size_t i = 1; i < 101; i++)
+       {
+    	fprintf(fp,"%d ",hist[i]);
+    	if(!(i%10))
+    	{
+    		fprintf(fp,"\n");
+    	}
+       }
     fclose(fp);
     fclose(fp2);
-    printf("%d\n %d\n %d\n %d\n %d %lf\n", min, max, mid, counter, counter2,avg);
+
+
     return 0;
 }
